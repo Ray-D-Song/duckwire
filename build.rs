02342e43c032da7701a7d14e227a9ad7c29b70b8
Ray-D-Song <songraysmail@gmail.com>
2026-04-29 17:35:20 +0800

    fix: windows link add Rstrtmgr and Bcrypt libs

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        println!("cargo:rustc-link-lib=rstrtmgr");
+    let target = std::env::var("TARGET").unwrap();
+    if target.contains("windows") {
+        println!("cargo:rustc-link-lib=Rstrtmgr");
+        println!("cargo:rustc-link-lib=Bcrypt");
     }
 }
